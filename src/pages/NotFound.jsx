009b51a900eaf0d02ce1e2437ed4992573efb13e
aruogunc <christian.aruogu@sterling.ng>
2025-06-17 22:27:40 +0100

    Correct error message

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const NotFound = () => (
     <div className="p-6 text-center">
         <h1 className="text-3xl font-bold mb-2">404 – Page Not Found</h1>
-        <p className="mb-4">Sorry, the page you’re looking for doesn’t exist.</p>
+        <p className="mb-4">Sorry, the page you are looking for doesn’t exist.</p>
         <Link to="/" className="text-blue-600 hover:underline">Go back home</Link>
     </div>
 );
